8f3ec3ed2fd6d7266bee47296ab246d64bf2c7f7
mariocstweb <mariocstmra@gmail.com>
2024-04-03 15:35:06 +0200

    Filled TypeProject controller

--- app/Http/Controllers/Api/TypeProjectController.php
+++ app/Http/Controllers/Api/TypeProjectController.php
@@ -3,64 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use App\Models\Type;
 use Illuminate\Http\Request;
 
+use function PHPUnit\Framework\returnSelf;
+
 class TypeProjectController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    //
+    public function __invoke(string $id)
     {
-        //
-    }
+        $type = Type::find($id);
+        if (!$type) return response(null, 404);
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+        $type->load('projects.type', 'projects.technologies');
+        $projects = $type->projects;
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Type $type)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Type $type)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Type $type)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Type $type)
-    {
-        //
+        return response()->json(['projects' => $projects, 'label' => $type->label]);
     }
 }
